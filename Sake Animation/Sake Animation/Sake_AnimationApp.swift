83efc8e231b953076f8bfcc6db185c448ffb2af5
Kekelifenuku <113697135+Kekelifenuku@users.noreply.github.com>
2022-10-04 19:12:23 +0000

    Add files via upload

---
+++ Sake Animation/Sake Animation/Sake_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sake_AnimationApp.swift
+//  Sake Animation
+//
+//  Created by kekeli on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sake_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
